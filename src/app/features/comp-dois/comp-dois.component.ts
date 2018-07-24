49a8c13fe2ed02c5bab2295a50be2f3718e19548
Alisson Santos de Souza <alissonssz@hotmail.com>
2018-07-23 22:54:27 -0300

    Destroying observables!

--- src/app/features/comp-dois/comp-dois.component.ts
+++ src/app/features/comp-dois/comp-dois.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import { Observer } from 'rxjs/Observer';
 
 @Component({
   selector: 'app-comp-dois',
   templateUrl: './comp-dois.component.html',
   styleUrls: ['./comp-dois.component.css']
 })
-export class CompDoisComponent implements OnInit {
-
+export class CompDoisComponent implements OnInit, OnDestroy {
+  subSegundoObservable: Subscription;
   constructor() { }
 
   ngOnInit() {
     this.criarSegundoObservable();
   }
   criarSegundoObservable () {
-    Observable.create();
+    this.subSegundoObservable =  Observable.create((observer: Observer<boolean>) => {
+      observer.next(true);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subSegundoObservable.unsubscribe();
   }
 
 }
--- src/app/features/comp-um/comp-um.component.ts
+++ src/app/features/comp-um/comp-um.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-comp-um',
   templateUrl: './comp-um.component.html',
   styleUrls: ['./comp-um.component.css']
 })
-export class CompUmComponent implements OnInit {
-
+export class CompUmComponent implements OnInit, OnDestroy {
+  
+  subPrimeiroObservable: Subscription;
   constructor() { }
 
   ngOnInit() {
@@ -15,8 +18,12 @@ export class CompUmComponent implements OnInit {
   }
 
   criarPrimeiroObservable () {
-    Observable.create();
-
+    this.subPrimeiroObservable =  Observable.create((observer: Observer<boolean>) => {
+      observer.next(true);
+    });
+  }
+  ngOnDestroy(): void {
+    this.subPrimeiroObservable.unsubscribe();
   }
 
 }
